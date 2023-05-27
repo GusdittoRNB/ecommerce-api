89254e86bfb208b11bf905a49436393df5190aa2
Ida Bagus Paalakaa RNB <114908038+GusdittoRNB@users.noreply.github.com>
2023-05-28 01:16:31 +0800

    Membuat Class DatabaseConnection

---
+++ ECommerceAPI/src/DatabaseConnection.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    public static Connection connect() {
+        Connection con = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            con = DriverManager.getConnection("jdbc:sqlite:db_ecommerce.db");
+            System.out.println("Connected!");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e + "");
+        }
+        return con;
+    }
+}
