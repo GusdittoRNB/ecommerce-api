7076e9d7d4f55d07b1a8670b364ef49ed250658d
Ida Bagus Paalakaa RNB <114908038+GusdittoRNB@users.noreply.github.com>
2023-05-29 01:57:40 +0800

    Update ECommerceAPI.java

--- ECommerceAPI/src/ECommerceAPI.java
+++ ECommerceAPI/src/ECommerceAPI.java
@@ -1,5 +1,19 @@
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpServer;
+
 public class ECommerceAPI {
+    private static final int PORT = 003;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
+            server.createContext("/users", new UserHandler());
+            server.setExecutor(null);
+            server.start();
+            System.out.println("Server started on port 00" + PORT);
+        } catch (Exception e) {
+            System.out.println("Error starting server: " + e.getMessage());
+        }
     }
 }
