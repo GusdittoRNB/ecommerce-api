f0d4640273cf2650a2ba61f4137b681cb0a1cd0c
Ida Bagus Paalakaa RNB <114908038+GusdittoRNB@users.noreply.github.com>
2023-05-30 03:13:05 +0800

    Update UserHandler.java

--- ECommerceAPI/src/UserHandler.java
+++ ECommerceAPI/src/UserHandler.java
@@ -5,10 +5,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.*;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class UsersHandler implements HttpHandler {
     private static void sendResponse(HttpExchange exchange, int statusCode, String response) throws IOException {
@@ -104,6 +101,65 @@ public class UsersHandler implements HttpHandler {
             e.printStackTrace();
             sendErrorResponse(exchange, 500, "Internal Server Error");
         }
+    }
+
+    private void handleGetUserById(HttpExchange exchange) throws IOException {
+//            if (!validateApiKey(exchange)) {
+//                sendErrorResponse(exchange, 401, "Unauthorized");
+//                return;
+//            }
+
+        String path = exchange.getRequestURI().getPath();
+        int userId = Integer.parseInt(path.substring(path.lastIndexOf('/') + 1));
+
+        try (Connection connection = DatabaseConnection.connect();
+             PreparedStatement statement = connection.prepareStatement(
+                     "SELECT users.user_id, users.first_name, users.last_name, users.email, " +
+                             "users.phone_number, users.type, addresses.address_id, addresses.type AS address_type, addresses.line1, " +
+                             "addresses.line2, addresses.city, addresses.province, addresses.postcode " +
+                             "FROM users LEFT JOIN addresses ON users.user_id = addresses.user_id " +
+                             "WHERE users.user_id = ?")) {
+
+            statement.setInt(1, userId);
+
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                JSONObject userObject = new JSONObject();
+                userObject.put("user_id", resultSet.getInt("user_id"));
+                userObject.put("first_name", resultSet.getString("first_name"));
+                userObject.put("last_name", resultSet.getString("last_name"));
+                userObject.put("email", resultSet.getString("email"));
+                userObject.put("phone_number", resultSet.getString("phone_number"));
+                userObject.put("type", resultSet.getString("type"));
+
+                JSONArray addressesArray = new JSONArray();
+                while (resultSet.getString("address_type") != null) {
+                    JSONObject addressObject = new JSONObject();
+                    addressObject.put("address_id", resultSet.getString("address_id"));
+                    addressObject.put("type", resultSet.getString("address_type"));
+                    addressObject.put("line1", resultSet.getString("line1"));
+                    addressObject.put("line2", resultSet.getString("line2"));
+                    addressObject.put("city", resultSet.getString("city"));
+                    addressObject.put("province", resultSet.getString("province"));
+                    addressObject.put("postcode", resultSet.getString("postcode"));
+                    addressesArray.put(addressObject);
+                    if (!resultSet.next()) {
+                        break;
+                    }
+                }
+
+                if (addressesArray.length() > 0) {
+                    userObject.put("addresses", addressesArray);
+                }
+
+                sendResponse(exchange, 200, userObject.toString());
+                return;
+            }
+        } catch (SQLException | JSONException e) {
+            e.printStackTrace();
+            sendErrorResponse(exchange, 500, "Internal Server Error");
+        }
+        sendErrorResponse(exchange, 404, "User not found");
     }
 
 }
