8f38d9106cb300281063936017d52a4ef458fd73
Ida Bagus Paalakaa RNB <114908038+GusdittoRNB@users.noreply.github.com>
2023-05-29 23:20:15 +0800

    Update UserHandler.java

--- ECommerceAPI/src/UserHandler.java
+++ ECommerceAPI/src/UserHandler.java
@@ -5,5 +5,30 @@ import org.json.JSONObject;
 import java.io.*;
 
 public class UsersHandler implements HttpHandler {
+    private static void sendResponse(HttpExchange exchange, int statusCode, String response) throws IOException {
+        exchange.getResponseHeaders().set("Content-Type", "application/json");
+        exchange.sendResponseHeaders(statusCode, response.length());
+        OutputStream outputStream = exchange.getResponseBody();
+        outputStream.write(response.getBytes());
+        outputStream.close();
+    }
 
+    private static void sendErrorResponse(HttpExchange exchange, int statusCode, String message) throws IOException {
+        JSONObject errorObject = new JSONObject();
+        errorObject.put("error", message);
+        sendResponse(exchange, statusCode, errorObject.toString());
+    }
+
+    private static String getRequestData(HttpExchange exchange) throws IOException {
+        InputStream inputStream = exchange.getRequestBody();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        StringBuilder requestData = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            requestData.append(line);
+        }
+
+        return requestData.toString();
+    }
 }
